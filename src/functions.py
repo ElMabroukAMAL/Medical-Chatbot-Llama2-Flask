4e079dde8b80cfdc9143370622213ba187b617d6
Amal <amalmabrouk0312@gmail.com>
2025-03-07 13:53:23 +0100

    add structure folder

---
+++ src/functions.py
@@ -0,0 +1,15 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+def load_data(data):
+    loader = DirectoryLoader(data, glob="*.pdf", loader_cls=PyPDFLoader)
+    return loader.load()
+
+def chunks(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    return text_splitter.split_documents(extracted_data)
+
+def download_embedding_model():
+    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
