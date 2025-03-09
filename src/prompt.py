231fbd62d81a754b452d64d04eb0f520723d7ac1
Amal <amalmabrouk0312@gmail.com>
2025-03-09 15:37:32 +0100

    add prompt

--- src/prompt.py
+++ src/prompt.py
@@ -1,6 +1,6 @@
 prompt_template="""
 Use the following information to answer the jusers question.
-If you don't know the answer, say that you don't know. don't make up an answer.
+If you don't know the answer, say that you don't know. Don't make up an answer.
 Context:{context}
 Question:{input}
 Only return the answer.
