d1543a7ca46e034e4f4e793838b9613fe0a276f4
Amal <amalmabrouk0312@gmail.com>
2025-03-09 15:22:44 +0100

    update README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Medical-Chatbot-Llama2-Flask
+# Project Overview
+
+A Flask-based medical chatbot using LangChain, Pinecone, and Meta's Llama 2 for efficient data retrieval and question answering.
+
+1. **Extract & Process Data** – Load medical PDFs and extract text.
+2. **Chunk Text** – Split content into manageable parts.
+3. **Generate Embeddings** – Convert chunks into vector representations using all-MiniLM-L6-v2.
+4. **Index Data** – Store embeddings in Pinecone for fast semantic search.
+5. **Build Knowledge Base** – Organize vectorized data for efficient retrieval.
+6. **User Query Processing** – Convert user questions into embeddings.
+7. **Search Knowledge Base** – Retrieve relevant document vectors.
+8. **Rank & Filter Results** – Select the most relevant text.
+9. **Generate Response** – Use Llama 2 to formulate accurate answers.
+10. **Deliver Answer** – Display response via Flask web app.
---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, jsonify, request
+from src.functions import download_embedding_model
+from src.prompt import *
+from langchain_pinecone import PineconeVectorStore
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+import os
+from dotenv import load_dotenv
+
+app =  Flask(__name__)
+
+# load my api env
+load_dotenv()
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+INDEX_NAME = os.environ.get('INDEX_NAME')
+
+# load my embedding model
+embedding_model = download_embedding_model()
+
+# load the index
+docsearch=PineconeVectorStore.from_existing_index(INDEX_NAME, embedding_model)
+
+# Create prompt template 
+prompt=PromptTemplate(template=prompt_template,input_variables=["context","input"])
+
+# Initialize the llm
+llm=CTransformers(model="llama-2-7b-chat.ggmlv3.q2_K.bin",model_type="llama")
+
+# Create a retriever that fetches the top 1 most relevant document for a given question
+retriever = docsearch.as_retriever(search_kwargs={'k': 2})
+
+# Set up a chain that combines retrieved documents into a single prompt for the llm
+QandA_chain = create_stuff_documents_chain(llm, prompt)
+
+# Combine the retriever and the question-answer chain into a single retrieval pipeline
+chain = create_retrieval_chain(retriever, QandA_chain)
+
+@app.route("/")
+def index():
+    return render_template('mchat.html')
+
+
+def ask_chat():
+    input=request.form["msg"]
+    result=chain.invoke({"context":'serious',"input":input})
+    print("Response : ", result)
+    print('type of result',type(result))
+    return str(result['answer'])
+
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000, debug=True)
